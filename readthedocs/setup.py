b2d6f5ad9ce00f14970b4860025ce7c414b58368
Aaron Caffrey <acaffrey@salesforce.com>
2018-07-23 14:16:23 +0100

    Added readthedocs code

---
+++ readthedocs/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+setup(name='ReadthedocsExample',
+      version='0.1.0',
+      description='A workshop for readthedocs.io',
+      url='https://github.com/caffalaughrey/ci-workshops',
+      author='Aaron Caffrey',
+      author_email='acaffrey@salesforce.com',
+      license='MIT',
+      packages=['ReadthedocsExample'],
+      package_dir = {'ReadthedocsExample': 'src'},
+      zip_safe=False,
+      install_requires=[
+          'pytest',
+          'pytest-runner',
+          'responses'],
+      tests_require=['pytest']
+      )
---
+++ readthedocs/tests/test_readthedocs.py
@@ -0,0 +1,9 @@
+import ReadthedocsExample
+
+
+def test_hello_world_works():
+    assert ReadthedocsExample.say_hello() == "Hello world!"
+
+
+def test_goodbye_world_works():
+    assert ReadthedocsExample.say_goodbye() == "Goodbye, cruel world!"
